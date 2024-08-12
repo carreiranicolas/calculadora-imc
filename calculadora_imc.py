2e43b7ec9d83ab3888007d0afae9c6dedd854f04
Nicolas Carreira <159592024+carreiranicolas@users.noreply.github.com>
2024-08-12 11:42:36 -0300

    Adicionando mudanças
    Adicionando algumas mudanças ao programa: fazendo um validador mais robusto e uma estrutura mais elegante para o programa.

--- calculadora_imc.py
+++ calculadora_imc.py
@@ -1,43 +1,72 @@
 while True:
-    try:
+   
+   #ENTRADA DE DADOS
 
-        peso = float(input('Digite seu peso (em KG): '))
-        altura = float(input('Digite sua altura (em metros): '))
+   peso = input("Digite seu peso em KG (ex: 67.2): ")
+   altura = input("Digite sua altura em metros (ex: 1.70): ")
 
-        imc = peso / altura**2
+   #VALIDADOR
+
+   try:
+      peso_float = float(peso)
+      altura_float = float(altura)
+      flag = True
+   except:
+      flag = None
+   
+   if flag == None:
+      print("Por favor, informe valores válidos.")
+      continue
+
+   if peso_float and altura_float != 0:
+      
+      #ALGORITMO
+       imc = peso_float / altura_float**2
        
-        o_3 = f'Seu imc é {imc:.2f} e seu nível é: Obesidade grau 3'
-        o_2 = f'Seu imc é {imc:.2f} e seu nível é: Obesidade grau 2'
-        o_1 = f'O seu imc é {imc:.2f} e seu nível é: Obesidade grau 1'
-        sp = f'O seu imc é {imc:.2f} e seu nível é: Sobrepeso'
-        n = f'Seu imc é {imc:.2f} e seu nível é: Normal'
-        abn = f'O seu imc é {imc:.2f} e seu nível é: Abaixo do normal'
+       o_3 = f'Seu imc é {imc:.2f} e seu nível é: Obesidade grau 3'
+       o_2 = f'Seu imc é {imc:.2f} e seu nível é: Obesidade grau 2'
+       o_1 = f'O seu imc é {imc:.2f} e seu nível é: Obesidade grau 1'
+       sp = f'O seu imc é {imc:.2f} e seu nível é: Sobrepeso'
+       n = f'Seu imc é {imc:.2f} e seu nível é: Normal'
+       abn = f'O seu imc é {imc:.2f} e seu nível é: Abaixo do normal'
 
-        if(imc > 40.0):
+       if(imc > 40.0):
          {
             print(o_3)
          }
-        elif(40.0 > imc >= 35.0):
+       elif(40.0 > imc >= 35.0):
          {
             print(o_2)
          }
-        elif(35.0 > imc >= 30.0):
+       elif(35.0 > imc >= 30.0):
          {
             print(o_1)
          }
-        elif(30.0 > imc >= 25.0):
+       elif(30.0 > imc >= 25.0):
          {
             print(sp)
          }
-        elif(25 > imc >= 18.6):
+       elif(25 > imc >= 18.6):
          {
             print(n)
          }
-        elif(imc <= 18.5):
+       elif(imc <= 18.5):
          {
             print(abn)
          }
-        break
-    except:
-      print("Verifique se você digitou os dados de entrada corretamente.")
+       else:
+          print("Ok, isso não deveria chegar até aqui")
+   else:
+      print("Por favor, informe valores diferentes de zero.")
+      continue
+  
+   #PARTE PARA SAIR DO PROGRAMA
+   
+   sair = input("Digite 's' para sair do programa: ").lower().startswith('s')
 
+   if sair == True:
+      print("Você saiu do programa.")
+      break
+
+
+ 
